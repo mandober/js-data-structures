9c981ac3d748a41b10458496083a263c98c60aa2
mandober <ivanilic1975@gmail.com>
2017-04-18 20:21:01 +0200

    small fix

--- stack/stack.js
+++ stack/stack.js
@@ -2,10 +2,10 @@
  * DATA STRUCTURES: STACK
  *
  * @description Stack implementation in JS
- * @version 0.0.170417
+ * @version 0.0.170418
  * @author Ivan Ilić <ivanilic1975@gmail.com>
  * @see https://github.com/mandober/js-data-structures
- * @summary This implementation comes with selectable storage type (object, array, map or set)
+ * @summary This implementation comes with selectable storage type (object, array, map or set).
  */
 
 
@@ -13,11 +13,19 @@
 /**
  * Construct new stack object.
  *
- * @param {number}    storageType  Type of underlying storage: 0=Object, 1=Array, 2=Map, 3=Set (defult: Object)
- * @returns {stack}  Returns the stack object.
+ * @constructor
+ * @typedef {(object)} Stack
+ * @property {number}  count         Stack elements counter.
+ * @property {number}  storageType   Keeps track about the chosen underlining JS storage (see 'storage' property).
+ * @property {object|array|map|set}  storage  Underlining JS storage used for storing stack elements.
+ * @this Stack
+ *
+ * @param {number}   storageType  Type of underlying storage: 0=Object, 1=Array, 2=Map, 3=Set (defult: 0)
+ * @returns {Stack}  Returns the stack object.
  */
 var Stack = function (storageType) {
     'use strict';
+    storageType = storageType || 0;
     // cater for the case when constructor is called without the `new` keyword
     if (this === undefined) return new Stack();
     this.count = 0;
@@ -25,23 +33,23 @@ var Stack = function (storageType) {
     switch (storageType) {
         case 0:
             this.storage = {};
-            this.storeType = 0;
+            this.storageType = 0;
             break;
         case 1:
             this.storage = [];
-            this.storeType = 1;
+            this.storageType = 1;
             break;
         case 2:
             this.storage = new Map();
-            this.storeType = 2;
+            this.storageType = 2;
             break;
         case 3:
             this.storage = new Set();
-            this.storeType = 3;
+            this.storageType = 3;
             break;
        default:
             this.storage = {};
-            this.storeType = 0;
+            this.storageType = 0;
     }
 }
 
@@ -50,17 +58,17 @@ var Stack = function (storageType) {
  * push() - add value to the stack.
  *
  * @param {*}  value  Value to add to the stack.
- * @returns {stack}  Returns the stack object.
+ * @returns {Stack}  Returns the stack object.
  */
 Stack.prototype.push = function (value) {
     'use strict';
     if (!value) return false;
     // object(0) and array(1):
-    if (this.storeType < 2) this.storage[this.count++] = value;
+    if (this.storageType < 2) this.storage[this.count++] = value;
     // map (2)
-    if (this.storeType === 2) this.storage.set(this.count++, value);
+    if (this.storageType === 2) this.storage.set(this.count++, value);
     // set (3):
-    if (this.storeType === 3) {
+    if (this.storageType === 3) {
         this.storage.add(value);
         this.count++;
     }
@@ -81,21 +89,21 @@ Stack.prototype.pop = function () {
     let val;
 
     // object(0) and array(1):
-    if (this.storeType < 2) {
+    if (this.storageType < 2) {
         val = this.storage[this.count - 1];
         delete this.storage[--this.count];
         // array(1) only
-        if (this.storeType === 1) this.storage.length = this.count;
+        if (this.storageType === 1) this.storage.length = this.count;
     }
 
     // map (get & delete by key):
-    if (this.storeType === 2) {
+    if (this.storageType === 2) {
         val = this.storage.get(this.count - 1);
         this.storage.delete(--this.count);
     }
 
     // set (get with iterator, delete by value):
-    if (this.storeType === 3) {
+    if (this.storageType === 3) {
         var set = this.storage;
         var it = set.keys();
         for (var i = 0, len = set.size - 1; i < len; ++i) {
@@ -120,17 +128,17 @@ Stack.prototype.peek = function () {
     if (this.count === 0) return false;
 
     // object(0) and array(0):
-    if (this.storeType === 0) {
+    if (this.storageType === 0) {
         return this.storage[this.count - 1];
     }
 
     // map (get  by key):
-    if (this.storeType === 2) {
+    if (this.storageType === 2) {
         return this.storage.get(this.count - 1);
     }
 
     // set (get with iterator):
-    if (this.storeType === 3) {
+    if (this.storageType === 3) {
         var set = this.storage;
         var it = set.keys();
         for (var i = 0, len = set.size - 1; i < len; ++i) {
@@ -144,7 +152,7 @@ Stack.prototype.peek = function () {
 /**
  * Swap   the two top most elements of the stack.
  *
- * @returns {stack}  Returns the stack object. On failure returns `false`.
+ * @returns {Stack}  Returns the stack object. On failure returns `false`.
  */
 Stack.prototype.swap = function () {
     'use strict';
@@ -163,13 +171,13 @@ Stack.prototype.swap = function () {
 /**
  * empty() - empties the stack.
  *
- * @returns {stack}  Returns the stack object.
+ * @returns {Stack}  Returns the stack object.
  */
 Stack.prototype.empty = function () {
     // reset count
     this.count = 0;
     // initialize new storage
-    switch (this.storeType) {
+    switch (this.storageType) {
         case 0:
             this.storage = {};
             break;
